c668939e61c1ce7593e0e99422771a84050fd3a3
lucas8306 <lucas84848486@gmail.com>
2025-12-02 20:07:51 -0300

    Update load_model.lua

--- load_model.lua
+++ load_model.lua
@@ -1,17 +1,5 @@
 local M = require("evoNN")
-
--- Carrega o modelo salvo
-local nn = M.loadModel("melhor_modelo.lua")
-
--- Define a entrada de teste
-local entrada = {0.5, 0.5}
-
--- Normalização
-nn:setNormalization({0, 0}, {1, 1}, {0}, {1})
-
--- Faz a predição
-local saida = nn:predict(entrada)
-
--- Exibe o resultado
-print("Entrada:", table.concat(entrada, ", "))
-print("Saída prevista:", table.concat(saida, ", "))
+local best = M.loadModel("melhor_modelo.lua")
+best:setNormalization({0,0}, {1,1}, {0}, {1})
+local out = best:predict({0.5,0.5})
+print("Saída do melhor modelo:", out[1])
